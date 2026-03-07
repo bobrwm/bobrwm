607412612b389332c2c49c9c73feeb6814f9851a
Uzair Aftab <git@uzaaft.me>
2026-03-07 16:31:47 +0100

    chore: format and cleanup

--- src/shim/shim.m
+++ src/shim/shim.m
@@ -1,52 +1,43 @@
-#import <AppKit/AppKit.h>
 #import "shim.h"
+#import <AppKit/AppKit.h>
 
-// ---------------------------------------------------------------------------
-// Accessibility
-// ---------------------------------------------------------------------------
-
-// ---------------------------------------------------------------------------
 // NSWorkspace observer
-// ---------------------------------------------------------------------------
-
 @interface BWObserver : NSObject
 @end
 
 @implementation BWObserver
 
 - (void)appLaunched:(NSNotification *)note {
-    NSRunningApplication *app = note.userInfo[NSWorkspaceApplicationKey];
-    pid_t pid = app.processIdentifier;
-    bw_workspace_app_launched(pid);
+  NSRunningApplication *app = note.userInfo[NSWorkspaceApplicationKey];
+  pid_t pid = app.processIdentifier;
+  bw_workspace_app_launched(pid);
 }
 
 - (void)appTerminated:(NSNotification *)note {
-    NSRunningApplication *app = note.userInfo[NSWorkspaceApplicationKey];
-    bw_workspace_app_terminated(app.processIdentifier);
+  NSRunningApplication *app = note.userInfo[NSWorkspaceApplicationKey];
+  bw_workspace_app_terminated(app.processIdentifier);
 }
 
 - (void)spaceChanged:(NSNotification *)note {
-    (void)note;
-    bw_workspace_space_changed();
+  (void)note;
+  bw_workspace_space_changed();
 }
 
 - (void)displayChanged:(NSNotification *)note {
-    (void)note;
-    bw_workspace_display_changed();
+  (void)note;
+  bw_workspace_display_changed();
 }
 
 - (void)activeAppChanged:(NSNotification *)note {
-    NSRunningApplication *app = note.userInfo[NSWorkspaceApplicationKey];
-    if (app) {
-        bw_workspace_active_app_changed(app.processIdentifier);
-    }
+  NSRunningApplication *app = note.userInfo[NSWorkspaceApplicationKey];
+  if (app) {
+    bw_workspace_active_app_changed(app.processIdentifier);
+  }
 }
 
 @end
 
-// ---------------------------------------------------------------------------
 // Status bar
-// ---------------------------------------------------------------------------
 
 @interface BWStatusBarDelegate : NSObject
 - (void)retile:(id)sender;
@@ -56,14 +47,14 @@
 @implementation BWStatusBarDelegate
 
 - (void)retile:(id)sender {
-    (void)sender;
-    bw_retile();
+  (void)sender;
+  bw_retile();
 }
 
 - (void)quit:(id)sender {
-    (void)sender;
-    bw_will_quit();
-    [NSApp terminate:nil];
+  (void)sender;
+  bw_will_quit();
+  [NSApp terminate:nil];
 }
 
 @end
